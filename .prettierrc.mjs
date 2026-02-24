579a0775519c06f7ed46c513d7d0d40ce5852f4f
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-02-24 12:18:21 +0100

    chore: add eslint, prettier
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

---
+++ .prettierrc.mjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  bracketSameLine: true,
+  bracketSpacing: false,
+  //plugins: ['prettier-plugin-organize-imports'],
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
+
+export default config;
