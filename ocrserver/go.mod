0a202b34c66f325c5de6dc412d7d385cac56a532
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-03-04 17:00:09 +0100

    feat: add ocrserver
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

---
+++ ocrserver/go.mod
@@ -0,0 +1,10 @@
+module github.com/godatei/datei/ocrserver
+
+go 1.26
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/otiai10/gosseract/v2 v2.4.1
+	github.com/spf13/pflag v1.0.10
+	gopkg.in/gographics/imagick.v3 v3.7.3
+)
---
+++ ocrserver/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"path"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/go-chi/chi"
+	"github.com/otiai10/gosseract/v2"
+	"github.com/spf13/pflag"
+	"gopkg.in/gographics/imagick.v3/imagick"
+)
+
+type Options struct {
+	Addr string
+}
+
+func (opts *Options) Bind(flags *pflag.FlagSet) {
+	flags.StringVar(&opts.Addr, "addr", ":8585", "Serve address")
+}
+
+func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	var opts Options
+	opts.Bind(pflag.CommandLine)
+	pflag.Parse()
+
+	r := chi.NewMux()
+	r.Post("/", handlePost)
+
+	slog.Info("server started", "addr", opts.Addr)
+	if err := http.ListenAndServe(opts.Addr, r); !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("serve error", "error", err)
+	}
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	// max memory 1GiB
+	const maxMultipartMemory = 1 << 30
+	if err := r.ParseMultipartForm(maxMultipartMemory); err != nil {
+		slog.Debug("error parsing multipart form", "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var lang []string
+	if langStr := r.FormValue("lang"); langStr != "" {
+		if err := json.Unmarshal([]byte(langStr), &lang); err != nil {
+			slog.Debug("error decoding lang", "error", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		slog.Debug("error getting multipart file", "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tmpDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		slog.Error("failed to create temp dir", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer os.RemoveAll(tmpDir)
+
+	tmpFile, err := os.Create(path.Join(tmpDir, "file"))
+	if err != nil {
+		slog.Error("failed to create temp file", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer tmpFile.Close()
+
+	if _, err := io.Copy(tmpFile, file); err != nil {
+		slog.Error("failed to copy file", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var ocrFiles []string
+	if contentType := fileHeader.Header.Get("Content-Type"); contentType == "application/pdf" {
+		ocrFiles, err = split(tmpFile.Name())
+		if err != nil {
+			slog.Error("error splitting PDF", "error", err)
+		}
+	} else {
+		ocrFiles = []string{tmpFile.Name()}
+	}
+
+	if len(ocrFiles) > 1 {
+		slices.SortFunc(ocrFiles, func(a, b string) int {
+			aa, err := strconv.Atoi(strings.TrimSuffix(strings.TrimPrefix(a, "page-"), ".jpg"))
+			if err != nil {
+				return -1
+			}
+			bb, err := strconv.Atoi(strings.TrimSuffix(strings.TrimPrefix(b, "page-"), ".jpg"))
+			if err != nil {
+				return 1
+			}
+			return aa - bb
+		})
+	}
+
+	out := new(strings.Builder)
+	for i, file := range ocrFiles {
+		slog.Debug("running ocr", "file", file)
+		out1, err := ocr(file, []string{"eng-best"}, "")
+		if err != nil {
+			slog.Debug("ocr error", "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if i != 0 {
+			// emulate page break
+			out.WriteString("\n\n---\n\n")
+		}
+		out.WriteString(out1)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(map[string]any{"text": out.String()}); err != nil {
+		slog.Debug("failed to write response", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func split(file string) ([]string, error) {
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	dir := path.Base(file)
+
+	_, err := imagick.ConvertImageCommand([]string{
+		"convert",
+		"-density", "300",
+		file,
+		path.Join(dir, "page.jpg"),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("ConvertImageCommand: %w", err)
+	}
+
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("ReadDir: %w", err)
+	}
+
+	var result []string
+	for _, entry := range dirEntries {
+		if !entry.IsDir() && strings.HasPrefix(entry.Name(), "page") {
+			result = append(result, path.Join(dir, entry.Name()))
+		}
+	}
+
+	return result, nil
+}
+
+func ocr(file string, langs []string, format string) (string, error) {
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	if err := client.SetImage(file); err != nil {
+		return "", fmt.Errorf("SetImage: %w", err)
+	}
+
+	if len(langs) > 0 {
+		if err := client.SetLanguage(langs...); err != nil {
+			return "", fmt.Errorf("SetLanguage: %w", err)
+		}
+	}
+
+	switch format {
+	case "hocr":
+		return client.HOCRText()
+	default:
+		return client.Text()
+	}
+}
