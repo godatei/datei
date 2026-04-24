a377fd870b41c282dd100205964d519252586a63
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-04-24 15:28:52 +0200

    chore: fix test
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/src/app/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { DashboardComponent } from '~/frontend/dashboard/dashboard.component';
 
@@ -10,7 +11,7 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DashboardComponent],
-      providers: [provideHttpClient()],
+      providers: [provideHttpClient(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
