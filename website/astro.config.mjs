fa24e388323eeeea6b3fe83e076791e92aaf4753
Philip Miglinci <pmig@glasskube.com>
2026-06-11 17:02:54 +0200

    docs(website): add website styling
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://godatei.com',
-  integrations: [sitemap(), mdx()],
+  integrations: [icon({include: {lucide: ['*']}}), sitemap(), mdx()],
 });
