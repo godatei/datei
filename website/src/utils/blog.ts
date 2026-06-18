2013264b67cd0ab6c847636ddd66557f51426c39
Philip Miglinci <pmig@glasskube.com>
2026-06-18 13:01:31 +0200

    chore: implemen feedback
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- website/src/utils/blog.ts
+++ website/src/utils/blog.ts
@@ -1,33 +1,15 @@
-import {getCollection, render, type RenderResult} from 'astro:content';
-import type {BlogPostConfig} from '../content.config';
-import type {Post} from '../types';
+import {getCollection, type CollectionEntry} from 'astro:content';
 
-const load = async function (): Promise<Array<Post>> {
-  return (await getCollection('blog'))
-    .map(
-      post =>
-        ({
-          data: post.data as BlogPostConfig,
-          rendered: render(post) as Promise<RenderResult>,
-        }) as Post,
-    )
-    .sort((a, b) => {
-      const dateA = a.data.publishDate
-        ? new Date(a.data.publishDate).valueOf()
-        : 0;
-      const dateB = b.data.publishDate
-        ? new Date(b.data.publishDate).valueOf()
-        : 0;
-      return dateB - dateA;
-    });
-};
-
-let _posts: Array<Post>;
+export type Post = CollectionEntry<'blog'>;
 
 export const getSortedPosts = async (): Promise<Array<Post>> => {
-  if (!_posts) {
-    _posts = await load();
-  }
-
-  return _posts;
+  return (await getCollection('blog')).sort((a, b) => {
+    const dateA = a.data.publishDate
+      ? new Date(a.data.publishDate).valueOf()
+      : 0;
+    const dateB = b.data.publishDate
+      ? new Date(b.data.publishDate).valueOf()
+      : 0;
+    return dateB - dateA;
+  });
 };
